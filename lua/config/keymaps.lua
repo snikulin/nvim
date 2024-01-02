5b4fc365ddf0181da0f4815aab292686c0218b51
Sergey Nikulin <snikulin@gmail.com>
2024-01-02 12:00:37 +0300

    Neogit added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local Util = require("lazyvim.util")
+local map = Util.safe_keymap_set
+local neogit = require("neogit")
+
+map("n", "<leader>gg", function()
+  neogit.open()
+end, { desc = "Open neogit" })
+map("n", "<leader>gG", function()
+  neogit.open()
+end, { desc = "Open neogit" })
